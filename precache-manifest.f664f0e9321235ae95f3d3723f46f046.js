912bca22c573556e2432a43bf0cc27a1b7343609
radeth <123radeth@gmail.com>
2019-02-05 23:52:15 +0100

    Updates

---
+++ precache-manifest.f664f0e9321235ae95f3d3723f46f046.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af0bf1497ad58103b57b",
+    "url": "/to-do-list/static/js/runtime~main.af0bf149.js"
+  },
+  {
+    "revision": "c5b72730a605ebe92704",
+    "url": "/to-do-list/static/js/main.c5b72730.chunk.js"
+  },
+  {
+    "revision": "8699310a68681f595078",
+    "url": "/to-do-list/static/js/1.8699310a.chunk.js"
+  },
+  {
+    "revision": "c5b72730a605ebe92704",
+    "url": "/to-do-list/static/css/main.266954c7.chunk.css"
+  },
+  {
+    "revision": "5a76f4833fb79d24079977574decf3b0",
+    "url": "/to-do-list/index.html"
+  }
+];
