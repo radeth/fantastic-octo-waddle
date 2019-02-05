57c7ef923aa1d3f70ddc04510b293880559f923e
radeth <123radeth@gmail.com>
2019-02-05 23:58:50 +0100

    bug fixed

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 class Error extends Component {
   render() {
-    return <h1>Error</h1>;
+    return (<div>
+      <h1>Page not found</h1>
+      <Link to="/">home</Link>
+    </div>);
   }
 }
 
